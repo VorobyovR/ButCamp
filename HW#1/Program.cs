1b71d2de96aff2d91fe344ebebce0907d2c80a1e
VorobyovR <vorobyovromanmail@gmail.com>
2022-08-04 14:18:18 +0300

    new main array

---
+++ HW#1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+string[] arrayFirst = {"Type", "5", "Badge_ID", "cost", "-25", "timeSheet", ":)", "company"};
+int arrayFirstLength = arrayFirst.Length;
+Console.Write("[" + string.Join(", ", arrayFirst) + "]");
+
+// int randomJ = new Random().Next(0, 4);
+// string[] arraySecond = new string[randomJ];
+
+// for (int i = 0; i < arraySecond.Length; i++)
+// {
+//     int randomN = new Random().Next(0, arrayFirstLength);
+//     arraySecond[i] = arrayFirst[randomN];
+// }
+
+// Console.Write(" -> " + "[" + string.Join(", ", arraySecond) + "]");
+// Console.ReadKey();
+
