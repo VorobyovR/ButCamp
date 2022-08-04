793583c7fc96a8c596cb635a24cdfa325a464f96
VorobyovR <vorobyovromanmail@gmail.com>
2022-08-04 14:20:18 +0300

    create elements in second array

--- HW#1/Program.cs
+++ HW#1/Program.cs
@@ -6,11 +6,11 @@ Console.Write("[" + string.Join(", ", arrayFirst) + "]");
 int randomJ = new Random().Next(0, 4);
 string[] arraySecond = new string[randomJ];
 
-// for (int i = 0; i < arraySecond.Length; i++)
-// {
-//     int randomN = new Random().Next(0, arrayFirstLength);
-//     arraySecond[i] = arrayFirst[randomN];
-// }
+for (int i = 0; i < arraySecond.Length; i++)
+{
+    int randomN = new Random().Next(0, arrayFirstLength);
+    arraySecond[i] = arrayFirst[randomN];
+}
 
 // Console.Write(" -> " + "[" + string.Join(", ", arraySecond) + "]");
 // Console.ReadKey();
